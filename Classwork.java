329f87425c65384c15adec9bfab3b6df69e9a964
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-27 10:52:28 -0700

    worked on

--- Classwork.java
+++ Classwork.java
@@ -6,5 +6,22 @@ public class Classwork
   }
 
   // write solutions to problems below
+  public static String firstLastN(String word, int n)
+  {
+    String output = new String();
+    String firstN = word.substring(0,n);
+    int indexNToLast = word.length() - n;
+    String lastN = word.substring(indecNToLast);
+
+    output = firstN + lastN;
+    return output;
+  }
+
+  public static String delEnd(String word. int n)
+  {
+    String output = new String();
+    int indTL = word.length() - n;
+    
+  }
 
 }
